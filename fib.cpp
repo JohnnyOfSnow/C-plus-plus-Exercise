266989554e119aedf2843fb3eef58ddea8e50db1
櫻雪紛飛 <good258johnny@yahoo.com.tw>
2015-03-02 12:07:10 +0800

    改成求費式數列黃金比值逼近

--- fib.cpp
+++ fib.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 
 using namespace std; // using direction, we don't need to write the std::cout
-int Fib(int number);
+double Fib(int number);
 
 int _tmain(int argc, _TCHAR* argv[]) // For 微軟的Visual Studio  c++ main's should be  _tmain
 {
@@ -15,19 +15,21 @@ int _tmain(int argc, _TCHAR* argv[]) // For 微軟的Visual Studio  c++ main's s
 	cout << "請輸入費伯納西，你要求幾項:";
 	cin >> fib;
 
-	cout << "費伯納西的數列為 \n";
+	cout << "開始計算油費伯納西數列所得的黃金比例逼近\n";
 
-	for (iterationCount = 0; iterationCount <= fib; iterationCount++){
-		cout << Fib(iterationCount) << endl;
+	for (iterationCount = 1; iterationCount <= (fib - 1); iterationCount++){
+		double ratio = Fib(iterationCount) / Fib(iterationCount + 1);
+		cout << Fib(iterationCount) << "/" << Fib(iterationCount + 1) << " = " << ratio << endl;
 	}
+
 
 	system("pause");
 	return 0;
 }
 
-int Fib(int number){
-	if (number == 0 || number == 1){ // base case
-		return 1;
+double Fib(int number){
+	if (number == 1 || number == 2){ // base case
+		return 1.0;
 	}else{
 		return Fib(number - 1) + Fib(number - 2); // recursively call
 	}
