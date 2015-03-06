4d4fe8a1d4184d5b785372c55b363b4ebc1db923
櫻雪紛飛 <good258johnny@yahoo.com.tw>
2015-03-06 13:17:59 +0800

    整數換二進制完成

--- NumberBaseChange.cpp
+++ NumberBaseChange.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 
 using namespace std; // using direction, we don't need to write the std::cout
-int conbination(int n, int m);
+void printBinaryNumber(int integer, double point);
 
 int _tmain(int argc, _TCHAR* argv[]) // For 微軟的Visual Studio  c++ main's should be  _tmain
 {
@@ -24,8 +24,37 @@ int _tmain(int argc, _TCHAR* argv[]) // For 微軟的Visual Studio  c++ main's s
 	cout << "整數部分為:" << entirePartition << endl;
 	cout << "小數部分為:" << pointPartition << endl;
 	
+	printBinaryNumber(entirePartition, pointPartition);
+
 	system("pause");
 	return 0;
+}
+
+void printBinaryNumber(int integer, double point){
+	int count = 1;
+	int div = 2;
+
+	while (integer >= div){
+		div = div * 2;
+		count = count + 1;
+	}
+
+	// Find the higest exp which stores in count
+	count = count - 1;
+	div = div / 2;
+
+	while (count >= 0){
+		if (integer / div != 0){
+			cout << "1";
+		}
+		else{
+			cout << "0";
+		}
+		integer = integer % div;
+		count = count - 1;
+		div = div / 2;
+	}
+	//cout << integer;
 }
 
 
