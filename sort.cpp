03140f6c5ac59cef2da1b82a7a55159d7fc8cad9
櫻雪紛飛 <good258johnny@yahoo.com.tw>
2015-03-13 12:44:31 +0800

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -8,7 +8,7 @@
 #define SIZE 10 // define a constant
 
 using namespace std; // using direction, we don't need to write the std::cout
-
+void insertionSort(int *array);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -18,12 +18,22 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	while (counter != SIZE){
 		coin[counter] = 1 + (rand() % 99); // pick random 1 value 
+		cout << coin[counter] << ", ";
 		counter = counter + 1;
-	
 	}
 
+	cout << endl;
 
 	system("pause");
 	return 0;
+}
+
+
+void insertionSort(int *array){
+
+
+
+
+
 }
 
